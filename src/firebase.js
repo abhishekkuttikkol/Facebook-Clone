643a7f8a1fc4908e393f3322647317ea91fd55e3
Abhishek P <75170385+abhishekkuttikkol@users.noreply.github.com>
2021-10-22 12:52:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCPgzP9VxTJ8fCd2Lfilf4ouxkSchewnPA",
-    authDomain: "facebook-mern-0015.firebaseapp.com",
-    projectId: "facebook-mern-0015",
-    storageBucket: "facebook-mern-0015.appspot.com",
-    messagingSenderId: "197263612165",
-    appId: "1:197263612165:web:0851daba1dbe677cd97f6c"
+    
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
